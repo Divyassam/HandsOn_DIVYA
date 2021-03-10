fba01dae680311640c98cbba4c696c2e300b3a82
Divya S Sam <divyassam@gmail.com>
2021-03-10 12:23:36 +0530

    Case Study NUnit

---
+++ Case Studies/NUnit_Final_MyFoodSupply/MyFoodSupply/MyFoodSupply/TestClass.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFoodSupply
+{
+    [TestFixture]
+    class TestClass
+    {
+        Program objMainProgram ;
+        FoodDetail food;
+        [SetUp]
+        public void Init()
+        {
+            objMainProgram = new Program();
+            food=objMainProgram.CreateFoodDetail("Burger", 1, Convert.ToDateTime("2021-10-20"), 100);
+        }
+
+        [Test]
+        [TestCase("Sandwich",1,"2021-10-20",100)] //Check food obj creation    
+        public void CreateFood_AllValid_Success(string name, int dishType, DateTime expiryDate, double price)
+        {
+             food = objMainProgram.CreateFoodDetail(name, dishType, expiryDate, price);
+             Assert.AreEqual(food.Name, name);
+             TestContext.WriteLine("Food Item created successfully!!");           
+        }
+
+        [Test]
+        [TestCase("", 1, "2021-10-20", 100)] //Exception -- name parameter NULL
+        public void CreateFood_NullName_Exception(string name, int dishType, DateTime expiryDate, double price)
+        {
+            var result = Assert.Throws<Exception>(() => objMainProgram.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.AreEqual("Dish name cannot be empty. Please provide valid value", result.Message);
+        }
+
+        [Test]
+        [TestCase("Sandwich", 1, "2021-10-20", 300)] //Exception -- price parameter < 0
+        public void CreateFood_InvalidPrice_Exception(string name, int dishType, DateTime expiryDate, double price)
+        {
+            var result = Assert.Throws<Exception>(() => objMainProgram.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.AreEqual("Incorrect value for dish price. Please provide valid value", result.Message);
+        }
+
+        [Test]
+        [TestCase("Sandwich", 1, "2017-10-20", 100)] //Exception -- expiryDate parameter  < currentdate
+        public void CreateFood_InvalidDate_Exception(string name, int dishType, DateTime expiryDate, double price)
+        {
+            var result = Assert.Throws<Exception>(() => objMainProgram.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.AreEqual("Incorrect expiry date. Please provide valid value", result.Message);
+        }
+
+
+        
+
+        [Test]
+        [TestCase(100,"2021-05-03", "Tom",250)] //Check Supply obj creation
+        public void CreateSupply_AllValid_Success(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            SupplyDetail supply = objMainProgram.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, food);
+            Assert.AreEqual(supply.Count, foodItemCount);
+            Assert.AreEqual(supply.RequestDate, requestDate);
+            Assert.AreEqual(supply.SellerName, sellerName);
+            Assert.AreEqual(supply.TotalCost, packingCharge + food.Price * foodItemCount);
+            Assert.AreSame(supply.FoodItem, food);
+            TestContext.WriteLine("Supply object created successfully");
+
+
+        }
+
+
+        [Test]
+        [TestCase(-10, "2021-05-03", "Tom", 250)]//Exception -- value less than 0 for food item count
+        public void CreateFood_NegativeItemCount_Exception(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            var result = Assert.Throws<Exception>(() => objMainProgram.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, food));
+            Assert.AreEqual("Incorrect food item count. Please check", result.Message);
+        }
+
+
+        [Test]
+        [TestCase(100, "2015-05-03", "Tom", 250)]//Exception -- date less than today
+        public void CreateFood_InvalidDate_Exception(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            var result = Assert.Throws<Exception>(() => objMainProgram.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, food));
+            Assert.Contains(result.Message, new List<string> { "Incorrect food request date. Please provide valid value", "Request date greater than expiry date. Please check" });
+        }
+
+
+        [Test]
+        [TestCase(10, "2021-05-03", "Tom", 250)] //Exception -- fooditem value NULL
+        public void CreateFood_NullItem_Exception(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            SupplyDetail supply = objMainProgram.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, null);
+            Assert.Null(supply);
+            TestContext.WriteLine("Null Supply");
+           
+        }
+
+    }
+}
---
+++ Case Studies/NUnit_Practice_GenericMedicine/GenericMedicine/GenericMedicine/TestClass.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMedicine
+{
+    [TestFixture]
+    class TestClass
+    {
+        public static Program objMainProgram;
+        public static Medicine medicine;
+        [SetUp]
+        public void Init()
+        {
+            objMainProgram = new Program();
+            medicine = objMainProgram.CreateMedicineDetail("Paracetamol", "Acetaminophen", "Potassium sorbate", Convert.ToDateTime("2023-10-20"), 20);
+        }
+
+        [Test]
+        [TestCase("Paracetamol", "Acetaminophen", "Potassium sorbate", "2023-10-20", 20)] //Check medicine obj creation
+
+        public void CreateMedicine_AllValid_Sucess(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+        {
+            medicine = objMainProgram.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip);
+            Assert.AreEqual(medicine.Name, name);
+            Assert.AreEqual(medicine.GenericMedicineName, genericMedicineName);
+            Assert.AreEqual(medicine.Composition, composition);
+            Assert.AreEqual(medicine.ExpiryDate, expiryDate);
+            Assert.AreEqual(medicine.PricePerStrip, pricePerStrip);
+            TestContext.WriteLine("Medicine object created successfully");
+
+        }
+
+
+        [Test]
+        [TestCase("Paracetamol", "", "Potassium sorbate", "2023-10-20", 20)] //Exception -- GenericMedicineName parameter NULL
+        public void CreateMedicine_nullName_Exception(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+        {
+            var result = Assert.Throws<Exception>(() => objMainProgram.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip));
+            Assert.AreEqual("Generic Medicine name cannot be empty. Please provide valid value", result.Message);
+        }
+
+
+        [Test]
+        [TestCase("Paracetamol", "Acetaminophen", "Potassium sorbate", "2023-10-20", -10)] //Exception -- price parameter < 0
+        public void CreateMedicine_negativePrice_Exception(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+        {
+            var result = Assert.Throws<Exception>(() => objMainProgram.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip));
+            Assert.AreEqual("Incorrect value for Medicine price per strip. Please provide valid value", result.Message);
+        }
+
+        [Test]
+        [TestCase("Paracetamol", "Acetaminophen", "Potassium sorbate", "2019-10-20", 20)] //Exception -- expiryDate parameter  < currentdate
+        public void CreateMedicine_invalidExpiryDate_Exception(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+        {
+            var result = Assert.Throws<Exception>(() => objMainProgram.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip));
+            Assert.AreEqual("Incorrect expiry date. Please provide valid value", result.Message);
+        }
+
+
+
+        [Test]
+        [TestCase(10,"2021-10-20", "London")] //Check carton obj creation
+        public void CreateCartone_AllValid_Sucess(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {          
+            CartonDetail cartonDetail = objMainProgram.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, null);
+            Assert.AreEqual(cartonDetail.MedicineStripCount, medicineStripCount);
+            Assert.AreEqual(cartonDetail.LaunchDate, launchDate);
+            Assert.AreEqual(cartonDetail.RetailerAddress, retailerAddress);
+            Assert.AreEqual(cartonDetail.Medicine, medicine);
+            TestContext.WriteLine("Medicine object created successfully");
+
+         }
+
+        [Test]
+        [TestCase(-5,"2021-10-20", "London")]//Exception -- value less than 0 for medicine strip count
+        public void CreateCarton_negativeStripCount_Exception(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+            var result = Assert.Throws<Exception>(() => objMainProgram.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, medicine));
+            Assert.AreEqual("Incorrect strip count. Please check", result.Message);
+        }
+
+
+        [Test]
+        [TestCase(10, "2024-03-07", "London")] //Exception --launch date greater than expiry date
+        public void CreateCarton_InvalidLaunchDate_Exception(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+            var result = Assert.Throws<Exception>(() => objMainProgram.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, medicine));
+            Assert.Contains(result.Message, new List<string> { "Incorrect launch date. Please provide valid value", "Launch date greater than expiry date. Please check"});
+        }
+
+
+        [Test]
+        [TestCase(10, "2021-10-20", "London")] //Exception -- medicine value NULL
+        public void CreateCarton_NullMedicine_Exception(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+            var result = objMainProgram.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, null);          
+            Assert.Null(result);
+            TestContext.WriteLine("Null carton");            
+        }
+
+    }
+}
